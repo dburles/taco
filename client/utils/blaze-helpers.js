e073e6612d3f0f01810a0cc2644ce7856fc65046
Matt Ireland <matt.ireland@mortgagechoice.com.au>
2015-07-26 22:06:03 +1200

    Moved contact form to modal

---
+++ client/utils/blaze-helpers.js
@@ -0,0 +1,31 @@
+var DateFormats = {
+    short: "DD MMMM YYYY",
+    long: "dddd DD.MM.YYYY HH:mm"
+};
+
+UI.registerHelper("formatDate", function(datetime, format) {
+    if (moment) {
+        f = DateFormats[format];
+        return moment(datetime).format(f);
+    }
+    else {
+        return datetime;
+    }
+});
+
+UI.registerHelper("selectedMatching", function(str1, str1) {
+    if (str1 == str2)
+        return "selected"
+    else
+        return "";
+
+});
+
+UI.registerHelper("sessionMatching", function(sessionName, str1) {
+    if (Session.get(sessionName) == str1)
+        return true
+    else
+        return false;
+
+});
+
--- client/views/home.js
+++ client/views/home.js
@@ -10,5 +10,30 @@ Template.home.events({
     'keyup #txtContactSearch': function (e, t) {
         event.preventDefault();
         Session.set("searchText", $("#txtContactSearch").val());
+    },
+    'click #newContactMenu': function (e, t) {
+        event.preventDefault();
+        //Session.set("action", "newContact");
+        Modal.show('contactModal');
+    }
+
+});
+
+Template.newContact.events({
+    'click #CancelContact': function (e, t) {
+        event.preventDefault();
+        Session.set("action");
     }
 });
+
+Template.contactModal.events({
+    'click #CancelContact': function (e, t) {
+        event.preventDefault();
+        Modal.hide("contactModal")
+    },
+    'click #SaveContact': function (e, t) {
+        //event.preventDefault();
+        Modal.hide("contactModal")
+    }
+});
+
