72170e53903f8e561b491e0c5d585962ace128c6
Matt Ireland <matt.ireland@mortgagechoice.com.au>
2015-10-23 22:10:33 +1000

    More on supplierdocuments

--- client/views/supplier-documents.js
+++ client/views/supplier-documents.js
@@ -8,7 +8,7 @@ Template.supplierDocuments.helpers({
     },
     documents: function() {
         var supplier = FlowRouter.getQueryParam('supplier');
-        return SupplierDocuments.find({});
+        return SupplierDocuments.find({supplier:supplier},{sort:{order:1}});
     }
 });
 
@@ -23,7 +23,8 @@ Template.supplierDocuments.events({
     },
     'click #add-document-button': function(e,t){
         event.preventDefault();
-        var doc = {};
+        var supplier = FlowRouter.getQueryParam('supplier')
+        var doc = {supplier:supplier};
         var supplierDocumentModalData = {
             type: "insert",
             doc: doc
@@ -53,5 +54,45 @@ Template.documentItem.events({
 });
 
 Template.supplierDocuments.onRendered(function () {
-    //do something...
+    setTimeout( function () {
+
+
+        this.$('.sortable-documents').sortable({
+            items: ".document-item:not(.unsortable)",
+            stop: function(e, ui) {
+                // get the dragged html element and the one before
+                //   and after it
+                el = ui.item.get(0)
+                before = ui.item.prev().get(0)
+                after = ui.item.next().get(0)
+
+                //  Blaze.getData takes as a parameter an html element
+                //    and will return the data context that was bound when
+                //    that html element was rendered!
+                var newRank;
+                var beforeRank;
+                var afterRank;
+                if (!before) {
+                    //if it was dragged into the first position grab the
+                    // next element's data context and subtract one from the rank
+                    newRank = Blaze.getData(after).order - 100 || Date.now();
+                } else if (!after) {
+                    //if it was dragged into the last position grab the
+                    //  previous element's data context and add one to the rank
+                    newRank = (Blaze.getData(before).order + 100) || Date.now()
+                }
+                else
+                {
+                    afterRank = Blaze.getData(after).order || Date.now();
+                    beforeRank = Blaze.getData(before).order || Date.now();
+                    newRank = Math.round( (beforeRank + afterRank) / 2);
+                }
+
+
+                //update the dragged Item's rank
+                SupplierDocuments.update({_id: Blaze.getData(el)._id}, {$set: {order: newRank}})
+
+            }
+        });
+    },100)
 });
