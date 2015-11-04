01a1d9e028dcbfbb770276d60f65005370347ecd
Matt Ireland <matt.ireland@mortgagechoice.com.au>
2015-11-04 22:52:26 +1000

    Majority of Overview in Factfind

--- client/views/profile.js
+++ client/views/profile.js
@@ -8,19 +8,134 @@ Template.profile.helpers({
         var id = FlowRouter.getParam("id");
         var docs = ProfileContacts.find({profileId:id});
         return docs;
+    },
+    properties: function(){
+        var id = FlowRouter.getParam("id");
+        var docs = ProfileProperties.find({profileId:id});
+        return docs;
+    },
+    mortgages: function(){
+        var id = FlowRouter.getParam("id");
+        var docs = ProfileLiabilities.find({profileId:id, type:'Mortgage'});
+        return docs;
+    },
+    loans: function(){
+        var id = FlowRouter.getParam("id");
+        var docs = ProfileLiabilities.find({profileId:id, type:'Loan'});
+        return docs;
+    },
+    cards: function(){
+        var id = FlowRouter.getParam("id");
+        var docs = ProfileLiabilities.find({profileId:id, type:'Card'});
+        return docs;
+    },
+    incomes: function(){
+        var profileContactId = this._id;
+        debugger;
+        var docs = ProfileIncomes.find({profileContactId:profileContactId});
+        return docs;
+    },
+    kids: function(){
+        var id = FlowRouter.getParam("id");
+        var docs = ProfileKids.find({profileId:id});
+        return docs;
+    },
+
+    formId: function(){
+        return 'profile-properties-' + this._id;
     }
 });
 
 Template.profile.events({
-    //'click #clearSearchButton': function (e, t) {
-    //    event.preventDefault();
-    //}
+    'click #add-property-button': function (e, t) {
+        event.preventDefault();
+        var id = FlowRouter.getParam("id");
+        ProfileProperties.insert({
+            profileId: id
+        })
+    },
+    'click .delete-property-button': function (e, t) {
+        event.preventDefault();
+        ProfileProperties.remove(this._id);
+    },
+
+    'click #add-mortgage-button': function (e, t) {
+        event.preventDefault();
+        var id = FlowRouter.getParam("id");
+        ProfileLiabilities.insert({
+            profileId: id,
+            type:'Mortgage',
+            frequency: 'Monthly'
+        })
+    },
+    'click .delete-mortgage-button': function (e, t) {
+        event.preventDefault();
+        ProfileLiabilities.remove(this._id);
+    },
+
+    'click #add-loan-button': function (e, t) {
+        event.preventDefault();
+        var id = FlowRouter.getParam("id");
+        ProfileLiabilities.insert({
+            profileId: id,
+            type:'Loan',
+            frequency: 'Monthly'
+        })
+    },
+    'click .delete-loan-button': function (e, t) {
+        event.preventDefault();
+        ProfileLiabilities.remove(this._id);
+    },
+
+    'click #add-card-button': function (e, t) {
+        event.preventDefault();
+        var id = FlowRouter.getParam("id");
+        ProfileLiabilities.insert({
+            profileId: id,
+            type:'Card'
+        })
+    },
+    'click .delete-liability-button': function (e, t) {
+        event.preventDefault();
+        ProfileLiabilities.remove(this._id);
+    },
+
+    'click .add-income-button': function (e, t) {
+        event.preventDefault();
+        var id = FlowRouter.getParam("id");
+        var profileContactId = this._id;
+        ProfileIncomes.insert({
+            profileId: id,
+            profileContactId: profileContactId
+        })
+    },
+    'click .delete-income-button': function (e, t) {
+        event.preventDefault();
+        ProfileIncomes.remove(this._id);
+    },
+
+    'click #add-kid-button': function (e, t) {
+        event.preventDefault();
+        var id = FlowRouter.getParam("id");
+        ProfileKids.insert({
+            profileId: id
+        })
+    },
+    'click #delete-kid-button': function (e, t) {
+        event.preventDefault();
+        var lastKid = ProfileKids.findOne({},{sort:{_id:-1}});
+        ProfileKids.remove(lastKid._id);
+    },
 });
 
 Template.profile.onCreated(function () {
     var id = FlowRouter.getParam("id");
     this.subscribe('profile', id);
     this.subscribe('profileContacts', id);
+    this.subscribe('profileProperties', id);
+    this.subscribe('profileLiabilities', id);
+    this.subscribe('profileIncomes', id);
+    this.subscribe('profileKids', id);
 });
 
 Template.profile.onRendered(function () {
--- server/profiles.js
+++ server/profiles.js
@@ -9,3 +9,23 @@ Meteor.publish("profileContacts", function(profileId){
     return docs;
 })
 
+Meteor.publish("profileProperties", function(profileId){
+    var docs = ProfileProperties.find({profileId:profileId});
+    return docs;
+})
+
+Meteor.publish("profileLiabilities", function(profileId){
+    var docs = ProfileLiabilities.find({profileId:profileId});
+    return docs;
+})
+
+Meteor.publish("profileIncomes", function(profileId){
+    var docs = ProfileIncomes.find({profileId:profileId});
+    return docs;
+})
+
+Meteor.publish("profileKids", function(profileId){
+    var docs = ProfileKids.find({profileId:profileId});
+    return docs;
+})
+
